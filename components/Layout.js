4746b88d57d4d04697e7496e84cc2ae1ead69103
Moshfeq <moshfeqshahrani@gmail.com>
2022-05-23 12:15:11 -0700

    updated NaviBar component label and added HeadInfo, NaviBar, and container components to Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,19 @@
-import Head from "next/head";
+import React from "react";
+import HeadInfo from "./HeadInfo";
+import NaviBar from  "./NaviBar";
+import { Container } from "semantic-ui-react";
 
+function Layout() {
+    return (
+        <div>
+            <HeadInfo />
+            <NaviBar />
+            <Container ></Container>
+        </div>
+    )
+}
+
+export default Layout;
 
 
 
