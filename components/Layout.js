b01a31619da283a6956b42b98222dfb9ca15b9f0
Moshfeq <moshfeqshahrani@gmail.com>
2022-05-23 16:01:24 -0700

    added nprogress library and added nprogress features to Layout.js but now needs css styling

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,16 @@ import React from "react";
 import HeadInfo from "./HeadInfo";
 import NaviBar from "./NaviBar";
 import { Container } from "semantic-ui-react";
+import nprogress from "nprogress";
+import Router from "next/router";
 
 function Layout({ children }) {
+
+Router.onRouteChangeStart=()=>nprogress.start();
+Router.onRouteChangeComplete=()=>nprogress.done();
+Router.onRouteChangeError=()=>nprogress.done();
+ 
+
     return (
         <div>
             <HeadInfo />
