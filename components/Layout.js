063e5c3d9cfe06cbe4bebc57e4acac9bbfa7d875
Moshfeq <moshfeqshahrani@gmail.com>
2022-05-23 14:27:03 -0700

    destructured Component in _app.js, added Layout and Component to _app.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,14 @@
 import React from "react";
 import HeadInfo from "./HeadInfo";
-import NaviBar from  "./NaviBar";
+import NaviBar from "./NaviBar";
 import { Container } from "semantic-ui-react";
 
-function Layout() {
+function Layout({ children }) {
     return (
         <div>
             <HeadInfo />
             <NaviBar />
-            <Container ></Container>
+            <Container style={{ paddingTop: "1rem" }} text></Container>
         </div>
     )
 }
--- components/NaviBar.js
+++ components/NaviBar.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Menu, Container, Icon } from "semantic-ui-react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 function NaviBar() {
     const router = useRouter()
+
+    
 }
 
 export default NaviBar;
