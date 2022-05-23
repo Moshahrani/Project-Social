371d54b3f96bdbe1a08628ebdeb381cfbcfcc5f6
Moshfeq <moshfeqshahrani@gmail.com>
2022-05-22 17:59:57 -0700

    added _app.js to pages folder for next.js and also added a public folder for styles.css as well as two components , Navbar and Layout to components folder

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {Menu, Container, Icon} from "semantic-ui-react";
+import {useRouter} from "next/router";
+
+function NaviBar() {
+    const router = useRouter()
+}
+
+export default NaviBar;
---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import App from "next/app";
+import "semantic-ui-css/semantic.min.css";
+
+class MyApp extends App {
+    render() {
+        
+    }
+}
+
+export default MyApp;
+
