2e9a9bbeff43ed33edbc49f7eaa413c84e4cf33a
Moshfeq <moshfeqshahrani@gmail.com>
2022-05-26 12:13:17 -0700

    added username field with regex test to check for proper format of username input

--- pages/signup.js
+++ pages/signup.js
@@ -2,89 +2,111 @@ import React, { useState, useEffect, useRef } from "react";
 import { Form, Button, Message, Segment, TextArea, Divider } from "semantic-ui-react";
 import { HeaderMessage, FooterMessage } from "../components/Basic/WelcomeMessage";
 
+
+// regex used to authenticate username info
+export const regexUserName = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/;
+
 function Signup() {
 
-const [user, setUser] = useState({
-    name: "",
-    email: "",
-    password: "",
-    bio: "", 
-    facebook: "",
-    youtube: "", 
-    twitter: "",
-    instagram: "",
-})
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+        bio: "",
+        facebook: "",
+        youtube: "",
+        twitter: "",
+        instagram: "",
+    })
 
-const { name, email, password, bio } = user;
+    const { name, email, password, bio } = user;
 
-const handleChange = (e) => {
-    const {name,value} = e.target
+    const handleChange = (e) => {
+        const { name, value } = e.target
 
-    setUser(prev => ({ ...prev, [name]: value }))
-};
+        setUser(prev => ({ ...prev, [name]: value }))
+    };
 
-const handleSubmit = e => e.preventDefault();
+    const handleSubmit = e => e.preventDefault();
 
-const [showSocialLinks, setShowSocialLinks] = useState(false);
-const [showPassword, setShowPassword] = useState(false);
-const [errorMessage, setErrorMessage] = useState(null);
-const [formLoading, setFormLoading] = useState(false);
-const [username, setUsername] = useState('');
-const [usernameLoading, setUsernameLoading] = useState(false);
-const [usernameAvailable, setUsernameAvailable] = useState(false);
-
+    const [showSocialLinks, setShowSocialLinks] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [formLoading, setFormLoading] = useState(false);
+    
+    const [username, setUsername] = useState('');
+    const [usernameLoading, setUsernameLoading] = useState(false);
+    const [usernameAvailable, setUsernameAvailable] = useState(false);
 
     return (
         <>
-        <HeaderMessage />
-        <Form loading={formLoading} error={errorMessage !== null} onSubmit={handleSubmit}>
-          <Message error header="Oops" content={errorMessage} onDismiss={() => setErrorMessage(null)} />
-          <Segment>
-              <Form.Input
-              required
-              fluid
-              label="Name"
-              placeholder="Name"
-              name="name"
-              value={name}
-              onChange={handleChange}
-              icon="user"
-              iconPosition="left"
-              />
-
-              <Form.Input
-              required
-              fluid
-              label="Email"
-              placeholder="Email"
-              name="email"
-              value={email}
-              onChange={handleChange}
-              icon="envelope"
-              iconPosition="left"
-              type="email"
-              />
-              <Form.Input
-              required
-              fluid
-              label="Password"
-              placeholder="Password"
-              name="password"
-              value={password}
-              onChange={handleChange}
-              icon={{
-                  name: "eye",
-                  circular: true,
-                  link: true,
-                  onClick: () => setShowPassword(!showPassword)
-
-              }}
-              iconPosition="left"
-              type={showPassword ? "text" : "password"}
-              />
-          </Segment>
-        </Form>
-        <FooterMessage />
+            <HeaderMessage />
+            <Form loading={formLoading} error={errorMessage !== null} onSubmit={handleSubmit}>
+                <Message error header="Oops" content={errorMessage} onDismiss={() => setErrorMessage(null)} />
+                <Segment>
+                    <Form.Input
+                        required
+                        fluid
+                        label="Name"
+                        placeholder="Name"
+                        name="name"
+                        value={name}
+                        onChange={handleChange}
+                        icon="user"
+                        iconPosition="left"
+                    />
+                    <Form.Input
+                        required
+                        fluid
+                        label="Email"
+                        placeholder="Email"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                        icon="envelope"
+                        iconPosition="left"
+                        type="email"
+                    />
+                    <Form.Input
+                        required
+                        fluid
+                        label="Password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                        icon={{
+                            name: "eye",
+                            circular: true,
+                            link: true,
+                            onClick: () => setShowPassword(!showPassword)
+                        }}
+                        iconPosition="left"
+                        type={showPassword ? "text" : "password"}
+                    />
+                     <Form.Input
+                        loading={usernameLoading}
+                        error={!usernameAvailable}
+                        required
+                        fluid
+                        label="Username"
+                        placeholder="Username"
+                        name="email"
+                        value={username}
+                        onChange={e =>  {
+                            setUsername(e.target.value)
+                          if(regexUserName.test(e.target.value)) {
+                              setUsernameAvailable(true);
+                          } else {
+                              setUsernameAvailable(false);
+                          } 
+                        }}
+                        icon={usernameAvailable ? "check" : "close"}
+                        iconPosition="left"
+                    />
+                </Segment>
+            </Form>
+            <FooterMessage />
         </>
     );
 };
