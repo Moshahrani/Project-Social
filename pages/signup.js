4a73b6d0c2db14a15a4e736b89ba2e1a8d2f67f8
Moshfeq <moshfeqshahrani@gmail.com>
2022-05-25 18:57:36 -0700

    added email, password inputs with their respective props as well as the handleSubmit & handleChange methods for signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -4,7 +4,7 @@ import { HeaderMessage, FooterMessage } from "../components/Basic/WelcomeMessage
 
 function Signup() {
 
-const [user, setUser]=useState({
+const [user, setUser] = useState({
     name: "",
     email: "",
     password: "",
@@ -17,6 +17,14 @@ const [user, setUser]=useState({
 
 const { name, email, password, bio } = user;
 
+const handleChange = (e) => {
+    const {name,value} = e.target
+
+    setUser(prev => ({ ...prev, [name]: value }))
+};
+
+const handleSubmit = e => e.preventDefault();
+
 const [showSocialLinks, setShowSocialLinks] = useState(false);
 const [showPassword, setShowPassword] = useState(false);
 const [errorMessage, setErrorMessage] = useState(null);
@@ -26,22 +34,53 @@ const [usernameLoading, setUsernameLoading] = useState(false);
 const [usernameAvailable, setUsernameAvailable] = useState(false);
 
 
-
-
     return (
         <>
         <HeaderMessage />
-        <Form loading={formLoading} error={errorMessage !== null} >
+        <Form loading={formLoading} error={errorMessage !== null} onSubmit={handleSubmit}>
           <Message error header="Oops" content={errorMessage} onDismiss={() => setErrorMessage(null)} />
           <Segment>
               <Form.Input
+              required
+              fluid
               label="Name"
               placeholder="Name"
               name="name"
               value={name}
-              //onChange={handleChange}
+              onChange={handleChange}
               icon="user"
               iconPosition="left"
+              />
+
+              <Form.Input
+              required
+              fluid
+              label="Email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              icon="envelope"
+              iconPosition="left"
+              type="email"
+              />
+              <Form.Input
+              required
+              fluid
+              label="Password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+              icon={{
+                  name: "eye",
+                  circular: true,
+                  link: true,
+                  onClick: () => setShowPassword(!showPassword)
+
+              }}
+              iconPosition="left"
+              type={showPassword ? "text" : "password"}
               />
           </Segment>
         </Form>
