031f0a2a802a577c22e037e0ec80004975a2a90c
Moshfeq <moshfeqshahrani@gmail.com>
2022-05-30 12:53:08 -0700

    added cloudinary credentials to next.config.js file as well as added an upload image method for cloudinary.js file under utilities

---
+++ utilities/cloudinary.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+const uploadImage = async (media) => {
+    try {
+        const form = new FormData()
+
+        form.append("file", media)
+        form.append("upload_preset", "project_social")
+        form.append("cloud_name", "moshhh")
+
+        const result = await axios.post(process.env.CLOUDINARY_URL, form)
+        return result.data.url
+
+    } catch (error) {
+        return
+    }
+}
