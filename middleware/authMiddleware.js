d3035a96306e64a2f2c08b3dfad31650a00634d9
Moshfeq <moshfeqshahrani@gmail.com>
2022-05-31 09:40:37 -0700

    added middleware folder with authMiddleware file where protecting the api routes will occur

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jsonwt = require("jsonwebtoken")
+
+
+module.exports = (req, res, next) => {
+    try {
+        if (!req.headers.authorization) {
+            return res.status(401).send("Unauthorized");
+        }
+         // destructuring userId from payload object after token was sent 
+         // from our backend (auth.js)
+
+        const { userId } = 
+            jsonwt.verify(req.headers.authorization,
+            process.env.jwtSecret);
+
+        req.userId = userId;
+        next();
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(401).send("Unauthorized");
+    }
+};
