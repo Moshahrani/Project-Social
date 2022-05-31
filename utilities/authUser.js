bdd825caf41d4ba1603a58afbe1eaaab326e6b7b
Moshfeq <moshfeqshahrani@gmail.com>
2022-05-31 11:36:41 -0700

    added redirectUser function for taking user to expected route or back to login page depending on token availability

--- utilities/authUser.js
+++ utilities/authUser.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import baseUrl from "./baseUrl";
 import catchErrors from "./catchErrors";
 import cookie from "js-cookie";
+import { Router } from "express";
 
 export const registerUser = async (user, profilePicUrl, setError, setLoading) => {
 
@@ -25,6 +26,18 @@ export const loginUser = async (user, setError, setLoading) => {
         setError(errorMessage);
     }
     setLoading(false);
+};
+
+export const redirectUser = (ctx, location) => {
+    // if user is on server-side
+    if (ctx.req) {
+        ctx.res.writeHead(302, { Location: location })
+        ctx.res.end();
+    } 
+    // user is on client-side
+    else {
+        Router.push(location);
+    }
 };
 
 // function to set cookie and push user to homepage
