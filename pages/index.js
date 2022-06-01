12b3449fac5a805d3d3a0496205f87f1a9c230bc
Moshfeq <moshfeqshahrani@gmail.com>
2022-06-01 13:28:08 -0700

    added welcome greeting to page's tab by using document.title when user is logged in and at the home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios"
 import baseUrl from "../utilities/baseUrl";
 
 function Index({ user, userFollowInfo }) {
-    console.log({ user, userFollowInfo })
+    
+  useEffect(() => {
+      document.title = `Welcome, ${user.name.split(" ")[0]}`;
+  }, []);
+
+
     return <div>Home</div>
 }
 
