eb26b28e166a1194e2d4961379dcddc86afc2d05
Moshfeq <moshfeqshahrani@gmail.com>
2022-06-01 19:42:27 -0700

    added search and sidemenu functional components with multiple grids in Layout.js with both scrollable and not scrollable sections

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { createRef } from "react";
 import HeadInfo from "./HeadInfo";
 import NaviBar from "./NaviBar";
-import { Container } from "semantic-ui-react";
+import { Container, Visibility, Grid, Sticky, Ref, Divider, Segment } from "semantic-ui-react";
 import nprogress from "nprogress";
 import Router from "next/router";
+import SideMenu from "./SideMenu";
+import Search from "./Search";
 
-function Layout({ children }) {
+function Layout({ children, user }) {
+    const contextRef = createRef()
+    // Router.onRouteChangeStart=()=>nprogress.start();
+    // Router.onRouteChangeComplete=()=>nprogress.done();
+    // Router.onRouteChangeError=()=>nprogress.done();
 
-// Router.onRouteChangeStart=()=>nprogress.start();
-// Router.onRouteChangeComplete=()=>nprogress.done();
-// Router.onRouteChangeError=()=>nprogress.done();
- 
 
     return (
         <>
             <HeadInfo />
-            <NaviBar />
-            <Container style={{ paddingTop: "1rem" }} text>
-                {children}
-            </Container>
+            {user ? (
+                <>
+                <div style={{ marginLeft: "1rem", marginRight: "1rem" }}>
+                  <Ref innerRef={contextRef}>
+                    <Grid>
+
+                        <Grid.Column floated="left" width={2}>
+                            <Sticky context={contextRef}>
+                                <SideMenu user={user} />
+                            </Sticky>
+                        </Grid.Column>
+                        
+                    </Grid>
+                  </Ref>
+                </div>
+                </>
+            ) : (
+                <>
+                <NaviBar />
+                <Container style={{ paddingTop: "1rem" }} text>
+                    {children}
+                </Container>
+                </>
+            )}
         </>
-    )
+    );
 }
 
 export default Layout;
