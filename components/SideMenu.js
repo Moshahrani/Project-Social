7cf130309553181f0ebbc006a848abce6164b75c
Moshfeq <moshfeqshahrani@gmail.com>
2022-06-02 21:38:05 -0700

    added notifications and username sections for Homepage

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -3,45 +3,73 @@ import { List, Icon } from "semantic-ui-react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-function SideMenu({ user: {username, email, unreadNotification, unreadMessage }}) {
+function SideMenu({ user: { username, email, unreadNotification, unreadMessage } }) {
     const router = useRouter();
 
     const isActive = route => router.pathname === route;
 
-  return <>
-     <List 
-     style={{ paddingTop: "1rem" }} 
-     size="big" 
-     verticalAlign="middle"
-     selection>
-        
-     <Link href="/">
-         <List.Item active={isActive("/")}>
-             <Icon name="home" size="large" color={isActive("/") && "green" } />
-             <List.Content>
-                 <List.Header content="Home" />
-             </List.Content>
-         </List.Item>
-     </Link>
-      
-     <Link href="/messages">
-         <List.Item active={isActive("/messages")}>
-             <Icon name={unreadMessage ? "hand point right" : "mail outline"}
-              size="large" 
-              color={(isActive("/messages") && "teal") || (unreadMessage && "orange")
-              }
-               />
-             <List.Content>
-                 <List.Header content="Messages" />
-             </List.Content>
-         </List.Item>
-     </Link>
+    return <>
+        <List
+            style={{ paddingTop: "1rem" }}
+            size="big"
+            verticalAlign="middle"
+            selection>
+
+            <Link href="/">
+                <List.Item active={isActive("/")}>
+                    <Icon name="home" size="large" color={isActive("/") && "green"} />
+                    <List.Content>
+                        <List.Header content="Home" />
+                    </List.Content>
+                </List.Item>
+            </Link>
+
+            <Link href="/messages">
+                <List.Item active={isActive("/messages")}>
+                    <Icon name={unreadMessage ? "hand point right" : "mail outline"}
+                        size="large"
+                        color={(isActive("/messages") && "teal") || (unreadMessage && "orange")
+                        }
+                    />
+                    <List.Content>
+                        <List.Header content="Messages" />
+                    </List.Content>
+                </List.Item>
+            </Link>
 
 
+            <Link href="/notifications">
+                <List.Item active={isActive("/notifications")}>
+                    <Icon name={unreadMessage ? "hand point right" : "bell outline"}
+                        size="large"
+                        color={(isActive("/messages") && "teal") || (unreadMessage && "orange")
+                        }
+                    />
+                    <List.Content>
+                        <List.Header content="Notifications" />
+                    </List.Content>
+                </List.Item>
+            </Link>
+
+            <Link href={`/${username}`}>
+                <List.Item active={router.query.username === username}>
+                    <Icon name="user" size="large" color={router.query.username === username && "green"} />
+                    <List.Content>
+                        <List.Header content="Account" />
+                    </List.Content>
+                </List.Item>
+            </Link>
+
+            <List.Item onClick={() => logout(email)}>
+                <Icon name="log out" size="large" />
+                <List.Content>
+                    <List.Header content="Logout" />
+                </List.Content>
+            </List.Item>
 
 
-     </List>
-     </>
+        </List>
+    </>
 }
 
 export default SideMenu
