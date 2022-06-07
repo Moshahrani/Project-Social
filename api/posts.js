6644ac6a5de31852eaaf8285f61573e3a145d1a6
Moshfeq <moshfeqshahrani@gmail.com>
2022-06-06 20:02:22 -0700

    delete post by ID post route implemented on api/posts.js

--- api/posts.js
+++ api/posts.js
@@ -70,4 +70,47 @@ router.get("/:postId", authMiddleware, async (req, res) => {
 
 })
 
+// delete post by ID
+
+router.delete("/:postId", authMiddleware, async (req, res) => {
+
+    try {
+
+        const { userId } = req;
+
+        const { postId } = req.params;
+
+        const post = await PostModel.findById(postId)
+
+        if (!post) {
+            return res.status(404).send("post not found");
+        }
+
+        const user = await UserModel.findById(userId);
+
+        // post.user is an object in post Schema Model so convert to string
+        // check if post is of current user
+
+        if (post.user.toString() !== userId) {
+
+            // user can only delete posts if root user
+            if (user.role === "root") {
+                await post.remove()
+                return res.status(200).send("Post deleted successfully");
+            } else {
+                return res.status(401).send("Unauthorized");
+            }
+        }
+        // if user is author of post, proceed to delete
+
+        await post.remove()
+        return res.status(200).send("Post deleted successfully");
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
+})
+
+
 module.exports = router;
