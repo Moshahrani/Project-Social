0f07043a7d711f0ce14ff11b809e72cdf2db2e27
Moshfeq <moshfeqshahrani@gmail.com>
2022-06-08 14:31:48 -0700

    added a noData component with multiple methods to let user know about no availability of specific requests

---
+++ components/NoData.js
@@ -0,0 +1,11 @@
+import { Message, Button } from "semantic-ui-react";
+
+// letting user know no posts are available
+export const NoPosts = () => (
+    <Message
+      info
+      icon="meh"
+      header="Hey!"
+      content="No Posts yet. Please follow someone!"
+    />
+  );
