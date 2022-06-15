ef2bd71c3776ce0a8336e7a4669d9e0c6b3da378
Moshfeq <moshfeqshahrani@gmail.com>
2022-06-14 20:40:25 -0700

    added NoProfile component message to NoData.js

--- components/NoData.js
+++ components/NoData.js
@@ -2,10 +2,15 @@ import { Message, Button } from "semantic-ui-react";
 
 // letting user know no posts are available
 export const NoPosts = () => (
-    <Message
-      info
-      icon="meh"
-      header="Hey!"
-      content="No Posts yet. Please follow someone!"
-    />
-  );
+  <Message
+    info
+    icon="meh"
+    header="Hey!"
+    content="No Posts yet. Please follow someone!"
+  />
+);
+
+
+export const NoProfile = () => (
+  <Message info icon="meh" header="Hey!" content="No Profile Found." />
+);
