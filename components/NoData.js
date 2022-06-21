a23e062761bd279e0390b29b5011a264366696b2
Moshfeq <moshfeqshahrani@gmail.com>
2022-06-21 15:42:49 -0700

    added noFollowData components for both followers and following

--- components/NoData.js
+++ components/NoData.js
@@ -10,17 +10,30 @@ export const NoPosts = () => (
   />
 );
 
-
+// letting user know that profile doesn't exist
 export const NoProfile = () => (
   <Message info icon="meh" header="Hey!" content="No Profile Found." />
 );
 
-// 
+// letting user know that no posts from that profile exist yet
 export const NoProfilePosts = () => (
   <>
     <Message info icon="meh" header="Sorry"
       content="User has not posted anything yet!" />
     <Button icon="long arrow alternate left" content="Go Back"
       as="a" href="/" />
+  </>
+);
+
+// letting user know that there's no follower/following data available for that profile
+export const NoFollowData = ({ followersComponent, followingComponent }) => (
+  <>
+    {followersComponent && (
+      <Message icon="user outline" info content={`User does not have followers`} />
+    )}
+
+    {followingComponent && (
+      <Message icon="user outline" info content={`User does not follow anyone`} />
+    )}
   </>
 );
