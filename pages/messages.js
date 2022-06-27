11043159ef51c280f50486e80e6abf991629e9e4
Moshfeq <moshfeqshahrani@gmail.com>
2022-06-27 12:33:39 -0700

    initial messsages layout

--- pages/messages.js
+++ pages/messages.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { useRouter } from "next/router";
 import baseUrl from "../utilities/baseUrl";
 import { parseCookies } from "nookies";
+import { Comment, Divider, Grid, Header, Icon, Segment } from "semantic-ui-react";
+import ChatList from "../components/Chats/Chat";
+import ChatListSearch from "../components/Chats/ChatListSearch";
+import { NoMessages } from "../components/NoData";
 
-function Messages({ chatsData }) {
-    
-    const [chats,setChats] = useState(chatsData)
+function Messages({ chatsData, user }) {
 
-    
+    const [chats, setChats] = useState(chatsData)
+    const router = useRouter();
+
+
     return (
-        <div>
+        <Segment basic padded size="large" style={{ marginTop: "5px" }}>
+            <Header
+                icon="home"
+                content="Go Back!"
+                onClick={() => router.push("/")}
+                style={{ cursor: "pointer" }} 
+                />
+            <Divider hidden />
 
-        </div>
+            <div style={{ marginBottom : "10px" }}>
+                <ChatListSearch user={user} chats={chats} setChats={setChats} />
+            </div>
+
+            {chats.length > 0 ? <> 
+
+            <>
+            <Grid stackable >
+                <Grid.Column width={4}>
+                    <Comment.Group size="big">
+                        <Segment raised style={{ overflow: "auto", maxHeight: "32rem" }}>
+                            {chats.map((chat, i) => (
+                                <ChatList 
+                                key={i}
+                                chat={chat}
+                                setChats={setChats}
+                                />
+                            ))}
+                        </Segment>
+                    </Comment.Group>
+                </Grid.Column>
+                </Grid></>
+            
+            
+            
+            </> : <NoMessages />}
+
+        </Segment>
     )
 }
 
@@ -31,3 +71,5 @@ Messages.getInitialProps = async (ctx) => {
         return { errorLoading: true }
     }
 }
+
+export default Messages;
