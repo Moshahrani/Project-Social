6b37e86bc0326d03a3236fff4810bc4045ccc583
Moshfeq <moshfeqshahrani@gmail.com>
2022-06-27 12:40:17 -0700

    added useEffect method

--- pages/messages.js
+++ pages/messages.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import baseUrl from "../utilities/baseUrl";
 import { parseCookies } from "nookies";
 import { Comment, Divider, Grid, Header, Icon, Segment } from "semantic-ui-react";
-import ChatList from "../components/Chats/Chat";
+import Chat from "../components/Chats/Chat";
 import ChatListSearch from "../components/Chats/ChatListSearch";
 import { NoMessages } from "../components/NoData";
 
@@ -12,7 +12,13 @@ function Messages({ chatsData, user }) {
 
     const [chats, setChats] = useState(chatsData)
     const router = useRouter();
-
+    
+    useEffect(() => {
+        if (chats.length > 0)
+          router.push(`/messages?message=${chats[0].messageWith}`, undefined, {
+            shallow: true
+          });
+    }, []);
 
     return (
         <Segment basic padded size="large" style={{ marginTop: "5px" }}>
@@ -20,35 +26,35 @@ function Messages({ chatsData, user }) {
                 icon="home"
                 content="Go Back!"
                 onClick={() => router.push("/")}
-                style={{ cursor: "pointer" }} 
-                />
+                style={{ cursor: "pointer" }}
+            />
             <Divider hidden />
 
-            <div style={{ marginBottom : "10px" }}>
+            <div style={{ marginBottom: "10px" }}>
                 <ChatListSearch user={user} chats={chats} setChats={setChats} />
             </div>
 
-            {chats.length > 0 ? <> 
+            {chats.length > 0 ? <>
 
-            <>
-            <Grid stackable >
-                <Grid.Column width={4}>
-                    <Comment.Group size="big">
-                        <Segment raised style={{ overflow: "auto", maxHeight: "32rem" }}>
-                            {chats.map((chat, i) => (
-                                <ChatList 
-                                key={i}
-                                chat={chat}
-                                setChats={setChats}
-                                />
-                            ))}
-                        </Segment>
-                    </Comment.Group>
-                </Grid.Column>
-                </Grid></>
-            
-            
-            
+                <>
+                    <Grid stackable >
+                        <Grid.Column width={4}>
+                            <Comment.Group size="big">
+                                <Segment raised style={{ overflow: "auto", maxHeight: "32rem" }}>
+                                    {chats.map((chat, i) => (
+                                        <Chat
+                                            key={i}
+                                            chat={chat}
+                                            setChats={setChats}
+                                        />
+                                    ))}
+                                </Segment>
+                            </Comment.Group>
+                        </Grid.Column>
+                    </Grid></>
+
+
+
             </> : <NoMessages />}
 
         </Segment>
