5d1665dab4944feedb599185f19f4f4377deccd6
Moshfeq <moshfeqshahrani@gmail.com>
2022-06-27 18:45:37 -0700

    added addUser and removeUser methods for online users availability through socket

---
+++ utilities/socialEvents.js
@@ -0,0 +1,38 @@
+
+const users = [];
+
+ // add user with socketId method
+ const addUser = async (userId, socketId) => {
+   const user = users.find(user => user.userId === userId);
+      // check if user already in array of users
+   if (user && user.socketId === socketId) {
+     return users;
+   }
+   
+    // remove user if not equal to socketId
+   else {
+     if (user && user.socketId !== socketId) {
+       await removeUser(user.socketId);
+     }
+     // create new user
+     const newUser = { userId, socketId };
+
+     users.push(newUser);
+
+     return users;
+   }
+ };
+
+
+ // method to remove user 
+ const removeUser = async socketId => {
+   const indexOf = users.map(user => user.socketId).indexOf(socketId);
+
+   await users.splice(indexOf, 1);
+
+   return;
+ };
+
+ const findConnectedUser = userId => users.find(user => user.userId === userId);
+
+ module.exports = { addUser, removeUser, findConnectedUser };
