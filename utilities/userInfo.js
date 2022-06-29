61de92ec8fc27eff9c8f99f3b8af3cb92e79b007
Moshfeq <moshfeqshahrani@gmail.com>
2022-06-28 17:38:48 -0700

    added userInfo method to retrieve User's Id

---
+++ utilities/userInfo.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import baseUrl from "./baseUrl";
+import cookie from "js-cookie";
+
+const userInfo = async (userToFindId) => {
+
+  try {
+
+    const result = await axios.get(`${baseUrl}/api/chats/user/${userToFindId}`, {
+        headers: { Authorization: cookie.get("token")}
+    });
+
+    return { name: result.data.name, profilePicUrl: result.data.profilePicUrl };
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default userInfo;
