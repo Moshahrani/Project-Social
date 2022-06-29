5c2e2beb77bed4f1e1c2e9bdb6d7a5207d86d51a
Moshfeq <moshfeqshahrani@gmail.com>
2022-06-28 17:41:44 -0700

    send message async operation added

--- utilities/messageEvents.js
+++ utilities/messageEvents.js
@@ -1,20 +1,20 @@
 const ChatModel = require("../models/ChatModel");
+// const UserModel = require("../models/UserModel");
 
-const loadMessages = async (userId, messageWith) => {
+const loadMessages = async (userId, messagesWith) => {
 
     try {
-          // receive user info from messagesWith of user and current user
+        // receive user info from messagesWith of user and current user
         const user = await ChatModel.findOne({ user: userId }).populate("chats.messagesWith")
 
-         // load messages with that particular user 
-        const chat = user.chats.find(chat => chats.messageWith._id.toString() === messagesWith)
-        
-        // if no chatß
+        // load messages with that particular user 
+        const chat = user.chats.find(chat => chat.messagesWith._id.toString() === messagesWith)
+
+        // if no chat
 
         if (!chat) {
             return { error: "No chat found" };
         }
-        
         return { chat };
 
     } catch (error) {
@@ -23,4 +23,59 @@ const loadMessages = async (userId, messageWith) => {
     }
 };
 
-module.exports = { loadMessages };
+
+const sendMsg = async (userId, msgSendToUserId, msg) => {
+
+    try {
+        // sender or logged in user 
+        const user = await ChatModel.findOne({ user: userId })
+        // receiver
+        const msgSendToUser = await ChatModel.findOne({ user: msgSendToUserId })
+
+        const newMsg = {
+            sender: userId,
+            receiver: msgSendToUserId,
+            msg,
+            date: Date.now()
+        }
+
+        // check for previous chat with user 
+        // if so, save message to that existing chat log
+        const prevChat = user.chats.find(chat => chat.messagesWith.toString() === msgSendToUserId)
+
+        if (prevChat) {
+            prevChat.messages.push(newMsg);
+            await user.save();
+        }
+        // else create whole new chat with ChatModel properties
+        else {
+
+            const newChat = { messagesWith: msgSendToUserId, messages: [newMsg] }
+            user.chats.unshift(newChat);
+            await user.save();
+        }
+
+       // same as above method but now for receiver of chat
+        const prevChatReceiver = msgSendToUser.chats.find(
+            chat => chat.messagesWith.toString() === userId)
+
+        if (prevChatReceiver) {
+            prevChatReceiver.messages.push(newMsg);
+            await msgSendToUser.save();
+        }
+        // else create whole new chat with ChatModel properties
+        else {
+            const newChat = { messagesWith: userId, messages: [newMsg] }
+            msgSendToUser.chats.unshift(newChat);
+            await msgSendToUser.save();
+        }
+        
+        return { newMsg }
+         
+    } catch (error) {
+        console.error(error); 
+        return { error }
+    }
+}
+
+module.exports = { loadMessages, sendMsg };
